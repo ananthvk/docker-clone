b8a79032e908f55fd73e5c6a0a7c8ce3903692e9
ananthvk <123456789987654321sr@gmail.com>
2023-12-04 02:21:48 +0530

    Modified the program to use clone(2)

--- config.h
+++ config.h
@@ -1,4 +1,5 @@
 #ifndef CONTAINER_CONFIG_H
+#define CONTAINER_CONFIG_H
 // DON'T forget the trailing slashes
 // path to store created containers
 #define CONTAINER_PATH "containers/"
@@ -6,5 +7,5 @@
 #define IMAGE_PATH "images/"
 #define CONTAINER_CACHE_PATH "cache/"
 #define CONTAINER_ID_LENGTH 10
-#define CONTAINER_CONFIG_H
+#define STACK_SIZE 8*1024*1024 // 8MiB of stack size for child process
 #endif
--- run.c
+++ run.c
@@ -19,6 +19,78 @@
  * @param argc number of arguments after run subcommand
  * @param argv arguments after the run subcommand, null terminated
  */
+struct container_args
+{
+    struct Container container;
+    int argc;
+    char **argv;
+};
+
+static int run_container(void *data)
+{
+    if (data == NULL)
+    {
+        printf("data was null, internal error");
+        exit(1);
+    }
+    struct container_args *c = (struct container_args *)(data);
+    struct Container container = (c->container);
+    int argc = c->argc;
+    char **argv = c->argv;
+    if (sethostname(container.id, container.id_length) == -1)
+    {
+        perror("Could not set hostname");
+        exit(1);
+    }
+    // Mount the root file system as private so that mount events do not propagate in and out of
+    // it
+    if (mount(NULL, "/", NULL, MS_PRIVATE | MS_REC, NULL) == -1)
+    {
+        perror("mount root");
+        exit(1);
+    }
+    container_extract_image(&container);
+    container_create_overlayfs(&container);
+    container_create_mounts(&container);
+
+    char path[PATH_MAX];
+    // TODO: Check if snprintf fails
+    snprintf(path, PATH_MAX, "%s/old-root%s", container.root, container.id);
+    mkdir(path, 0777);
+
+    if (syscall(SYS_pivot_root, container.root, path) == -1)
+    {
+        perror("Pivot root");
+        exit(1);
+    }
+
+    if (chdir("/") == -1)
+    {
+        perror("chdir");
+        exit(1);
+    }
+
+    // Remove old mount
+    snprintf(path, PATH_MAX, "/old-root%s", container.id);
+    if (umount2(path, MNT_DETACH) == -1)
+    {
+        perror("umount2");
+        exit(1);
+    }
+
+    if (rmdir(path) == -1)
+    {
+        perror("rmdir");
+        exit(1);
+    }
+
+    if (execvp(argv[1], argv + 1))
+    {
+        perror("execvp");
+        exit(1);
+    }
+    return 0;
+}
 
 void cmd_run(int argc, char *argv[])
 {
@@ -33,91 +105,35 @@ void cmd_run(int argc, char *argv[])
     container.image_name = argv[0];
     container.images_path = IMAGE_PATH;
     container.id_length = CONTAINER_ID_LENGTH;
-
-
     printf("=> Creating container\n");
     container_create(&container);
     printf("=> Created container %s [%s] \n", container.image_name, container.id);
-    // Fork the process and create the container
-    pid_t pid = fork();
+    // Clone the process and create the container
+    struct container_args data;
+    data.argc = argc;
+    data.argv = argv;
+    data.container = container;
+
+    char *stack = safe_malloc(STACK_SIZE);
+    char *stack_top = stack + STACK_SIZE; // Since stack grows downwards
+
+    // Clone - new namespace, new uts for a new hostname, sigchld so that the parent is notified
+    // if the child exits
+    pid_t pid = clone(&run_container, stack_top, CLONE_NEWNS | CLONE_NEWUTS | SIGCHLD, (void *)&data);
     if (pid == -1)
     {
-        perror("fork, container creation");
+        perror("clone, container creation");
         exit(1);
     }
-    if (pid == 0)
-    {
-        // Unshare to create new namespace for new mounts
-        // also set CLONE_NEWUTS for setting new hostname
-        if (unshare(CLONE_NEWNS | CLONE_NEWUTS) == -1)
-        {
-            perror("unshare: could not create new namespace, are you running as root?");
-            exit(1);
-        }
-        if (sethostname(container.id, container.id_length) == -1)
-        {
-            perror("Could not set hostname");
-            exit(1);
-        }
-        // Mount the root file system as private so that mount events do not propagate in and out of
-        // it
-        if (mount(NULL, "/", NULL, MS_PRIVATE | MS_REC, NULL) == -1)
-        {
-            perror("mount root");
-            exit(1);
-        }
-        container_extract_image(&container);
-        container_create_overlayfs(&container);
-        container_create_mounts(&container);
-
-        char path[PATH_MAX];
-        // TODO: Check if snprintf fails
-        snprintf(path, PATH_MAX, "%s/old-root%s", container.root, container.id);
-        mkdir(path, 0777);
-
-        if (syscall(SYS_pivot_root, container.root, path) == -1)
-        {
-            perror("Pivot root");
-            exit(1);
-        }
-
-        if (chdir("/") == -1)
-        {
-            perror("chdir");
-            exit(1);
-        }
-
-        // Remove old mount
-        snprintf(path, PATH_MAX, "/old-root%s", container.id);
-        if (umount2(path, MNT_DETACH) == -1)
-        {
-            perror("umount2");
-            exit(1);
-        }
-
-        if (rmdir(path) == -1)
-        {
-            perror("rmdir");
-            exit(1);
-        }
-
-        if (execvp(argv[1], argv + 1))
-        {
-            perror("execvp");
-            exit(1);
-        }
-    }
-    else
-    {
-        int status;
-        waitpid(pid, &status, 0);
-        container_delete(&container);
-        // Free resources
-        free(container.id);
-        free(container.root);
-        free(container.container_dir);
-        free(container.image_path);
-    }
+    int status;
+    waitpid(pid, &status, 0);
+    container_delete(&container);
+    // Free resources
+    free(container.id);
+    free(container.root);
+    free(container.container_dir);
+    free(container.image_path);
+    free(stack);
 }
 
 // sudo debootstrap --arch amd64 jammy images/ubuntu 'http://archive.ubuntu.com/ubuntu/
