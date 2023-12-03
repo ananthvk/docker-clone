df622bba8092ec8ab3260b9a71e5489f967660ea
ananthvk <123456789987654321sr@gmail.com>
2023-12-04 02:23:34 +0530

    modified config

--- config.h
+++ config.h
@@ -1,11 +1,9 @@
 #ifndef CONTAINER_CONFIG_H
 #define CONTAINER_CONFIG_H
-// DON'T forget the trailing slashes
 // path to store created containers
-#define CONTAINER_PATH "containers/"
+#define CONTAINER_PATH "containers"
 // path which contains the images
-#define IMAGE_PATH "images/"
-#define CONTAINER_CACHE_PATH "cache/"
+#define IMAGE_PATH "images"
 #define CONTAINER_ID_LENGTH 10
 #define STACK_SIZE 8*1024*1024 // 8MiB of stack size for child process
 #endif
